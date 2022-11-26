fc0ad11e749f45d36ac7237831e9913467d62144
LENOVO <LENOVO@DESKTOP-CJVF7TQ>
2022-11-26 22:55:08 +0300

    interface demo

---
+++ interfaceDemo/src/interfaceDemo/Main.java
@@ -0,0 +1,11 @@
+package interfaceDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		IWorkable workable = new Worker();
+		workable.work();
+
+	}
+
+}
---
+++ interfaceDemo/src/interfaceDemo/Worker.java
@@ -0,0 +1,17 @@
+package interfaceDemo;
+
+public class Worker implements IWorkable,IEatable{
+
+	@Override
+	public void work() {
+		System.out.println("Worker work");
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
